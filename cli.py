0ab667ce45aaa10fd3913eee2d51cd1e7b45da15
Phenguin <jcullen@mit.edu>
2012-09-13 05:20:21 -0400

    More modifications.. done now

--- cli.py
+++ cli.py
@@ -1,11 +1,13 @@
 from helpers import generate_complete_tmux_script
 import argparse
 import os
+from shutil import copyfile
 from glob import glob
 
 config_dir = "/home/jcullen/.pythontmux"
 template_ext = ".json"
 script_ext = ".pymux"
+default_template_name = "default"
 
 def templates_glob(config_dir_location):
     return os.path.join(get_templates_dir(config_dir_location), "*" + template_ext)
@@ -50,6 +52,37 @@ def update(config_dir):
 
     return get_script_names(config_dir)
 
+def run_template(config_dir, template_name):
+    return os.system("sh %s" % (get_script_full_path(config_dir, template_name),))
+
+def remove_template(config_dir, template_name):
+    print "Removing template and script for: %s.." % (template_name, )
+    try:
+        os.remove(get_template_full_path(config_dir, template_name))
+    except OSError:
+        print "Template %s doesnt exist, doing nothing" % (template_name,)
+
+    try:
+        os.remove(get_script_full_path(config_dir, template_name))
+    except OSError:
+        print "Script %s doesnt exist, doing nothing" % (template_name,)
+
+def template_exists(config_dir, template_name):
+    try:
+        with open(get_template_full_path(config_dir, template_name)):
+            return True
+    except IOError:
+        return False
+
+# XXX: Nicer errors
+def edit_template(config_dir, template_name):
+
+    if not template_exists(config_dir, template_name):
+        default_template_path = get_template_full_path(config_dir, default_template_name)
+        copyfile(default_template_path, get_template_full_path(config_dir, template_name))
+
+    return os.system("$EDITOR %s" % (get_template_full_path(config_dir, template_name),))
+
 def run(options):
     templates_dir = get_templates_dir(options.configdir)
     scripts_dir = get_scripts_dir(options.configdir)
@@ -57,19 +90,21 @@ def run(options):
     existing_scripts = update(options.configdir)
 
     if options.command == 'edit':
-        os.system("$EDITOR %s" % (os.path.join(templates_dir, options.template + ".json"),))
+        edit_template(options.configdir, options.template)
+    elif options.command == 'remove':
+        remove_template(options.configdir, options.template)
     elif options.command == 'run':
-        os.system("sh %s" % (os.path.join(scripts_dir,
-            options.template + ".pymux"),))
+        run_template(options.configdir, options.template)
     elif options.command == 'show':
         for name in existing_scripts:
-            print name
+            if name != default_template_name:
+                print name
     else:
         print "Unknown command: %s" % options.command
 
 def main():
     parser = argparse.ArgumentParser(description='Run tmux sessions defined by json configuration files')
-    parser.add_argument("command", choices = ["run", "edit", "show"])
+    parser.add_argument("command", choices = ["run", "edit", "show", "remove"])
     parser.add_argument("template",
             default="",
             type=str,
